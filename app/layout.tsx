d6c974e7ceaee3be9770e4121b3a983e5fc0435c
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2023-11-18 11:32:55 +0330

    space-animated-portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import StarsCanvas from '@/_components/StarBackground'
+import Navbar from '@/_components/Navbar'
+import Footer from '@/_components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className}  bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
